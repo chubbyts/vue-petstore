fe882ed66fafd48192085a213f3f6232fe73e8b8
Dominik Zogg <dominik.zogg@ikmail.com>
2025-08-13 20:45:56 +0200

    use-query

--- tests/component/page/pet/update.test.tsx
+++ tests/component/page/pet/update.test.tsx
@@ -5,29 +5,11 @@ import { render, screen } from '@testing-library/vue';
 import { userEvent } from '@testing-library/user-event';
 import { defineComponent } from 'vue';
 import { createRouter, createWebHistory, RouterView } from 'vue-router';
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query';
+import nock from 'nock';
 import type { PetRequest, PetResponse } from '../../../../src/model/pet';
 import { formatHtml } from '../../../formatter';
 import type { HttpError } from '../../../../src/client/error';
-import { NotFound, UnprocessableEntity } from '../../../../src/client/error';
-import type { readPetClient, updatePetClient } from '../../../../src/client/pet';
-
-// eslint-disable-next-line functional/no-let
-let mockReadPetClient: typeof readPetClient;
-// eslint-disable-next-line functional/no-let
-let mockUpdatePetClient: typeof updatePetClient;
-
-vi.mock('../../../../src/client/pet', () => {
-  return {
-    // eslint-disable-next-line functional/prefer-tacit
-    readPetClient: (id: string) => {
-      return mockReadPetClient(id);
-    },
-    // eslint-disable-next-line functional/prefer-tacit
-    updatePetClient: (id: string, pet: PetRequest) => {
-      return mockUpdatePetClient(id, pet);
-    },
-  };
-});
 
 vi.mock('../../../../src/component/form/pet-form', () => {
   return {
@@ -54,11 +36,13 @@ vi.mock('../../../../src/component/form/pet-form', () => {
 
 describe('update', () => {
   test('not found', async () => {
-    mockReadPetClient = async (id: string) => {
-      expect(id).toBe('4d783b77-eb09-4603-b99b-f590b605eaa9');
-
-      return new Promise<NotFound>((resolve) => resolve(new NotFound({ title: 'title' })));
-    };
+    nock('https://petstore.test').get('/api/pets/4d783b77-eb09-4603-b99b-f590b605eaa9').reply(404, {
+      type: 'https://datatracker.ietf.org/doc/html/rfc2616#section-10.4.5',
+      status: 404,
+      title: 'Not Found',
+      _httpError: 'NotFound',
+      detail: 'There is no entry with id "4d783b77-eb09-4603-b99b-f590b605eaa9"',
+    });
 
     const router = createRouter({
       history: createWebHistory(),
@@ -79,7 +63,21 @@ describe('update', () => {
 
     const { container } = render(<RouterView />, {
       global: {
-        plugins: [router],
+        plugins: [
+          [
+            VueQueryPlugin,
+            {
+              queryClient: new QueryClient({
+                defaultOptions: {
+                  queries: {
+                    retry: false,
+                  },
+                },
+              }),
+            },
+          ],
+          router,
+        ],
       },
     });
 
@@ -88,30 +86,31 @@ describe('update', () => {
     await screen.findByTestId('page-pet-update');
 
     expect(formatHtml(container.outerHTML)).toMatchInlineSnapshot(`
-    "<div>
-      <div data-testid="page-pet-update">
-        <div data-testid="http-error" class="mb-6 bg-red-300 px-5 py-4">
-          <p class="font-bold">title</p>
-          <!----><!----><!---->
+      "<div>
+        <div data-testid="page-pet-update">
+          <div data-testid="http-error" class="mb-6 bg-red-300 px-5 py-4">
+            <p class="font-bold">Not Found</p>
+            <p>There is no entry with id "4d783b77-eb09-4603-b99b-f590b605eaa9"</p>
+            <!----><!---->
+          </div>
+          <h1 class="mb-4 border-b border-gray-200 pb-2 text-4xl font-black">
+            Pet Update
+          </h1>
+          <!----><a
+            href="/pet"
+            class="inline-block px-5 py-2 text-white bg-gray-600 hover:bg-gray-700"
+            colortheme="gray"
+            >List</a
+          >
         </div>
-        <h1 class="mb-4 border-b border-gray-200 pb-2 text-4xl font-black">
-          Pet Update
-        </h1>
-        <!----><a
-          href="/pet"
-          class="inline-block px-5 py-2 text-white bg-gray-600 hover:bg-gray-700"
-          colortheme="gray"
-          >List</a
-        >
       </div>
-    </div>
-    "
-  `);
+      "
+    `);
   });
 
   test('default', async () => {
     const petResponse: PetResponse = {
-      id: '4d783b77-eb09-4603-b99b-f590b605eaa9',
+      id: '0a006ee6-7840-439a-8801-cee1d0f1e692',
       createdAt: '2005-08-15T15:52:01+00:00',
       updatedAt: '2005-08-15T15:55:01+00:00',
       name: 'Brownie',
@@ -120,11 +119,7 @@ describe('update', () => {
       _links: {},
     };
 
-    mockReadPetClient = async (id: string) => {
-      expect(id).toBe('4d783b77-eb09-4603-b99b-f590b605eaa9');
-
-      return new Promise<PetResponse>((resolve) => resolve(petResponse));
-    };
+    nock('https://petstore.test').get('/api/pets/0a006ee6-7840-439a-8801-cee1d0f1e692').reply(200, petResponse);
 
     const router = createRouter({
       history: createWebHistory(),
@@ -145,41 +140,55 @@ describe('update', () => {
 
     const { container } = render(<RouterView />, {
       global: {
-        plugins: [router],
+        plugins: [
+          [
+            VueQueryPlugin,
+            {
+              queryClient: new QueryClient({
+                defaultOptions: {
+                  queries: {
+                    retry: false,
+                  },
+                },
+              }),
+            },
+          ],
+          router,
+        ],
       },
     });
 
-    await router.push('/pet/4d783b77-eb09-4603-b99b-f590b605eaa9/update');
+    await router.push('/pet/0a006ee6-7840-439a-8801-cee1d0f1e692/update');
 
     await screen.findByTestId('page-pet-update');
 
     expect(formatHtml(container.outerHTML)).toMatchInlineSnapshot(`
-    "<div>
-      <div data-testid="page-pet-update">
-        <!---->
-        <h1 class="mb-4 border-b border-gray-200 pb-2 text-4xl font-black">
-          Pet Update
-        </h1>
-        <button
-          data-testid="pet-form-submit"
-          data-has-http-error="false"
-          data-has-initial-pet="true"
-        ></button
-        ><a
-          href="/pet"
-          class="inline-block px-5 py-2 text-white bg-gray-600 hover:bg-gray-700"
-          colortheme="gray"
-          >List</a
-        >
+      "<div>
+        <div data-testid="page-pet-update">
+          <!---->
+          <h1 class="mb-4 border-b border-gray-200 pb-2 text-4xl font-black">
+            Pet Update
+          </h1>
+          <button
+            data-testid="pet-form-submit"
+            data-has-http-error="false"
+            data-has-initial-pet="true"
+          ></button
+          ><a
+            href="/pet"
+            class="inline-block px-5 py-2 text-white bg-gray-600 hover:bg-gray-700"
+            colortheme="gray"
+            >List</a
+          >
+        </div>
       </div>
-    </div>
-    "
-  `);
+      "
+    `);
   });
 
   test('unprocessable entity', async () => {
     const petResponse: PetResponse = {
-      id: '4d783b77-eb09-4603-b99b-f590b605eaa9',
+      id: '796348ce-c637-4096-8d8d-238368924d2f',
       createdAt: '2005-08-15T15:52:01+00:00',
       updatedAt: '2005-08-15T15:55:01+00:00',
       name: 'Brownie',
@@ -188,19 +197,17 @@ describe('update', () => {
       _links: {},
     };
 
-    mockReadPetClient = async (id: string) => {
-      expect(id).toBe('4d783b77-eb09-4603-b99b-f590b605eaa9');
+    nock('https://petstore.test').get('/api/pets/796348ce-c637-4096-8d8d-238368924d2f').reply(200, petResponse);
 
-      return new Promise<PetResponse>((resolve) => resolve(petResponse));
-    };
-
-    mockUpdatePetClient = async (id: string) => {
-      expect(id).toBe('4d783b77-eb09-4603-b99b-f590b605eaa9');
-
-      return new Promise<UnprocessableEntity>((resolve) =>
-        resolve(new UnprocessableEntity({ title: 'unprocessable entity' })),
-      );
-    };
+    nock('https://petstore.test')
+      .put('/api/pets/796348ce-c637-4096-8d8d-238368924d2f', { name: 'Brownie', vaccinations: [] })
+      .reply(422, {
+        type: 'https://datatracker.ietf.org/doc/html/rfc2616#section-10.4.5',
+        status: 422,
+        title: 'Unprocessable Entity',
+        _httpError: 'UnprocessableEntity',
+        detail: 'Field validation issues',
+      });
 
     const router = createRouter({
       history: createWebHistory(),
@@ -221,11 +228,25 @@ describe('update', () => {
 
     const { container } = render(<RouterView />, {
       global: {
-        plugins: [router],
+        plugins: [
+          [
+            VueQueryPlugin,
+            {
+              queryClient: new QueryClient({
+                defaultOptions: {
+                  queries: {
+                    retry: false,
+                  },
+                },
+              }),
+            },
+          ],
+          router,
+        ],
       },
     });
 
-    await router.push('/pet/4d783b77-eb09-4603-b99b-f590b605eaa9/update');
+    await router.push('/pet/796348ce-c637-4096-8d8d-238368924d2f/update');
 
     const testButton = await screen.findByTestId('pet-form-submit');
 
@@ -234,30 +255,31 @@ describe('update', () => {
     await screen.findByTestId('http-error');
 
     expect(formatHtml(container.outerHTML)).toMatchInlineSnapshot(`
-    "<div>
-      <div data-testid="page-pet-update">
-        <div data-testid="http-error" class="mb-6 bg-red-300 px-5 py-4">
-          <p class="font-bold">unprocessable entity</p>
-          <!----><!----><!---->
+      "<div>
+        <div data-testid="page-pet-update">
+          <div data-testid="http-error" class="mb-6 bg-red-300 px-5 py-4">
+            <p class="font-bold">Unprocessable Entity</p>
+            <p>Field validation issues</p>
+            <!----><!---->
+          </div>
+          <h1 class="mb-4 border-b border-gray-200 pb-2 text-4xl font-black">
+            Pet Update
+          </h1>
+          <button
+            data-testid="pet-form-submit"
+            data-has-http-error="true"
+            data-has-initial-pet="true"
+          ></button
+          ><a
+            href="/pet"
+            class="inline-block px-5 py-2 text-white bg-gray-600 hover:bg-gray-700"
+            colortheme="gray"
+            >List</a
+          >
         </div>
-        <h1 class="mb-4 border-b border-gray-200 pb-2 text-4xl font-black">
-          Pet Update
-        </h1>
-        <button
-          data-testid="pet-form-submit"
-          data-has-http-error="true"
-          data-has-initial-pet="true"
-        ></button
-        ><a
-          href="/pet"
-          class="inline-block px-5 py-2 text-white bg-gray-600 hover:bg-gray-700"
-          colortheme="gray"
-          >List</a
-        >
       </div>
-    </div>
-    "
-  `);
+      "
+    `);
   });
 
   test('successful', async () => {
@@ -268,26 +290,18 @@ describe('update', () => {
     };
 
     const petResponse: PetResponse = {
-      id: '4d783b77-eb09-4603-b99b-f590b605eaa9',
+      id: 'fd115a9b-c4df-4bc0-ae2a-8b59b00502f2',
       createdAt: '2005-08-15T15:52:01+00:00',
       updatedAt: '2005-08-15T15:55:01+00:00',
       ...petRequest,
       _links: {},
     };
 
-    mockReadPetClient = async (id: string) => {
-      expect(id).toBe('4d783b77-eb09-4603-b99b-f590b605eaa9');
+    nock('https://petstore.test').get('/api/pets/fd115a9b-c4df-4bc0-ae2a-8b59b00502f2').reply(200, petResponse);
 
-      return new Promise<PetResponse>((resolve) => resolve(petResponse));
-    };
-
-    mockUpdatePetClient = async (id: string, petRequest: PetRequest) => {
-      expect(id).toBe('4d783b77-eb09-4603-b99b-f590b605eaa9');
-
-      expect(petRequest).toEqual(petRequest);
-
-      return new Promise<PetResponse>((resolve) => resolve(petResponse));
-    };
+    nock('https://petstore.test')
+      .put('/api/pets/fd115a9b-c4df-4bc0-ae2a-8b59b00502f2', { name: 'Brownie', vaccinations: [] })
+      .reply(200, petResponse);
 
     const router = createRouter({
       history: createWebHistory(),
@@ -308,11 +322,25 @@ describe('update', () => {
 
     const { container } = render(<RouterView />, {
       global: {
-        plugins: [router],
+        plugins: [
+          [
+            VueQueryPlugin,
+            {
+              queryClient: new QueryClient({
+                defaultOptions: {
+                  queries: {
+                    retry: false,
+                  },
+                },
+              }),
+            },
+          ],
+          router,
+        ],
       },
     });
 
-    await router.push('/pet/4d783b77-eb09-4603-b99b-f590b605eaa9/update');
+    await router.push('/pet/fd115a9b-c4df-4bc0-ae2a-8b59b00502f2/update');
 
     const testButton = await screen.findByTestId('pet-form-submit');
 
@@ -321,8 +349,8 @@ describe('update', () => {
     await screen.findByTestId('page-pet-list-mock');
 
     expect(formatHtml(container.outerHTML)).toMatchInlineSnapshot(`
-    "<div><div data-testid="page-pet-list-mock"></div></div>
-    "
-  `);
+      "<div><div data-testid="page-pet-list-mock"></div></div>
+      "
+    `);
   });
 });
